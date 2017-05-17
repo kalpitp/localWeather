5dd76d69598ad3b475612fcf3cfd76c0fa174a4d
kalpitp <kalpit.parikh@outlook.com>
2017-05-17 19:13:34 -0400

    switched to https API

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 $(document).ready(function() {
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+   if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
 
-      $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=e015ce3a48ba29605503b788374f551a", function(json) {
+ $.getJSON("https://api.darksky.net/forecast/ba71549f97e07b21bb1614407e38ffd5/"+  position.coords.latitude+","+ position.coords.longitude + "?callback=?", function(json) {
+     console.log(json)
+     var tempf= json.currently.apparentTemperature;
+     measurementSystem="fahrenheit";
 
-        var tempf = parseInt(json.main.temp * (9 / 5) - 459.67);
-        measurementSystem = "fahrenheit";
+     var tempc= (tempf-32)*1.8;
 
-        var tempc = parseInt(json.main.temp - 273);
+      htmlTempF= "The current temperature is "+tempf+ " degrees "+ " fahrenheit.";
+     htmlTempC= "The current temperature is "+tempc+ " degrees "+ "celsius.";
 
-        htmlTempF = "The current temperature is " + tempf + " degrees " + " fahrenheit.";
-        htmlTempC = "The current temperature is " + tempc + " degrees " + "celsius.";
+     var icon=json.currently.summary;
 
-        var icon = json.weather[0].main;
+     if (icon=="Clouds")
+       {
+        iconString="http://www.clipartbest.com/cliparts/KTj/e9x/KTje9x88c.png";
+        htmlIcon= "<img src="+iconString+" height="+100+"width="+100+"></img>"
+       }
+    else if (icon=="Rain")
+      {
+        iconString="http://www.mikeafford.com/store/store-images/ms01b_example_heavy_rain_showers.png";
+        htmlIcon= "<img src="+iconString+" height="+100+"width="+100+"></img>"
+      }
+     else
+       {
+         iconString="http://angourieblueseven.com/images/weather_sun.png";
+        htmlIcon= "<img src="+iconString+" height="+100+"width="+100+"></img>"
 
-        if (icon == "Clouds") {
-          iconString = "http://www.clipartbest.com/cliparts/KTj/e9x/KTje9x88c.png";
-          htmlIcon = "<img src=" + iconString + " height=" + 100 + "width=" + 100 + "></img>"
-        } else if (icon == "Rain") {
-          iconString = "http://www.mikeafford.com/store/store-images/ms01b_example_heavy_rain_showers.png";
-          htmlIcon = "<img src=" + iconString + " height=" + 100 + "width=" + 100 + "></img>"
-        } else {
-          iconString = "http://angourieblueseven.com/images/weather_sun.png";
-          htmlIcon = "<img src=" + iconString + " height=" + 100 + "width=" + 100 + "></img>"
-
-        }
+       }
 
 
-        $(".cardData").html(htmlTempF);
-        $(".cardIcon").html(htmlIcon);
-      });
-    });
-  }
+  $(".cardData").html(htmlTempF);
+     $(".cardIcon").html(htmlIcon);
+          });
+  });
+}
 
-  $("#measure").on("click", function() {
+     $("#measure").on("click", function(){
 
-    if (this.checked) {
+              if (this.checked) {
 
-      inCorF = htmlTempF;
+          inCorF=htmlTempF;
 
     } else {
-      inCorF = htmlTempC;
+          inCorF=htmlTempC;
     }
-    $(".cardData").html(inCorF);
-  });
+       $(".cardData").html(inCorF);
+     });
 
-});
+      });
